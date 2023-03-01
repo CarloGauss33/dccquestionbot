9d5e8162a292ee451cd3b1cefc6354328d9efdb6
Carlos Paredes <cparedesr@uc.cl>
2023-03-01 17:11:04 -0300

    feat(OpenAI): add chat support

--- src/openai.ts
+++ src/openai.ts
@@ -1,14 +1,18 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-import { Configuration, OpenAIApi } from "openai";
+import { Configuration, OpenAIApi, type ChatCompletionRequestMessage } from "openai";
 
 const TEMPERATURE = 0.15;
 const MAX_TOKENS = 300;
 const COMPLETION_MODEL = "text-davinci-003";
+const CHAT_MODEL = "gpt-3.5-turbo";
 const BASE_PROMPT = "Soy un bot altamente inteligente de la Pontificia Universidad Católica de Chile PUC que responde preguntas generales, computación e ingeniería. " +
                     "En caso de requerir responder con código utilizare <code> </code> para marcar el inicio y el fin del código. " +
                     "En caso de requerir negrita utilizare <b> </b> para marcar el inicio y el fin de la negrita. "
+
+const BASE_SYSTEM_CHAT = "Eres un estudiante de la Pontificia Universidad Católica de Chile PUC que responde preguntas generales, computación e ingeniería. "
+
 
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
@@ -18,6 +22,19 @@ const openai = new OpenAIApi(configuration);
 
 function build_prompt(question: string) {
     return `${BASE_PROMPT}\nQ: ${question}\nA: `;
+}
+
+function build_chat_messages(question: string) {
+    return [
+        {
+         'role': 'system',
+         'content': `${BASE_SYSTEM_CHAT}`
+        },
+        {
+         'role': 'user',
+         'content': `${question}`
+        }
+    ] as ChatCompletionRequestMessage[];
 }
 
 async function getCompletions(prompt: string) {
@@ -38,4 +55,21 @@ async function getCompletions(prompt: string) {
     }
 }
 
-export { getCompletions };
+async function getChatAnswer(question: string, username: string="") {
+    const request = {
+        model: CHAT_MODEL,
+        temperature: 0.25,
+        user: username,
+        messages: build_chat_messages(question)
+    };
+
+    const response = await openai.createChatCompletion(request);
+
+    if (response.data.choices) {
+        return response.data.choices[0].message?.content;
+    } else {
+        return "No se pudo obtener una respuesta";
+    }
+}
+
+export { getCompletions, getChatAnswer };
